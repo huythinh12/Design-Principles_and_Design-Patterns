e0ed6ca2cfcfc050e93edcf84b119a1d59325ada
Lariod <nguyenhuythinh7@gmail.com>
2021-07-17 05:03:31 +0700

    Fix commit Strategy Dessign Pattern

---
+++ DesignPattern/Strategy/Dragon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Strategy
+{
+    public class Dragon : Character,IMovementStrategy
+    {
+      
+        public string Move()
+        {
+            return "Movement type: Flying";
+        }
+    }
+}
